87ac6a6b75976e09c1b5ffa3a89fab570e188bae
AkiraOshima <151144469+AkiraOshima@users.noreply.github.com>
2023-11-20 14:18:34 +0900

    Update fixed_info.js

--- fixed_info.js
+++ fixed_info.js
@@ -1,5 +1,5 @@
 var type_and_serial = "No serial number";
-var dne_version = "Not included in sample files.";
+var dne_version = "DNE4.8.0.2.0";
 var dne_option = "Not included in sample files.";
 var mqtt_host = "";
 var mqtt_port = "";
