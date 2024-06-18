28cd713850f792e8b6a52239f2a6f6e3ef44ebf3
AkiraOshima <akira.oshima@doog-inc.com>
2024-06-18 15:12:13 +0900

    DNE5.1.1.0.0

--- DNE5.1.1.0.0-v20240617/fixed_info.js
+++ DNE5.1.1.0.0-v20240617/fixed_info.js
@@ -1,5 +1,5 @@
 var type_and_serial = "No serial number";
-var dne_version = "DNE5.0.3.0.0";
+var dne_version = "DNE5.1.1.0.0";
 var dne_option = "Not included in sample files.";
 var mqtt_host = "";
 var mqtt_port = "";
